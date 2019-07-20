a7058a016100695f82895601ad757c93e64ec36f
simbaking029 <simbaking028@gmail.com>
2019-07-20 18:49:37 +0300

    Insert social buttons

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,14 @@
 		align-center
 	>
 		<div class="text-xs-center full-width">
+			<div class="my-3">
+				<span>Get started with</span>
+			</div>
+			<div class="my-3 social">
+				<social-btn for="fb" />
+				<span>or</span>
+				<social-btn for="inst" />
+			</div>
 			<v-btn
 				class="full-width"
 				color="primary"
@@ -18,17 +26,30 @@
 
 <style lang="stylus">
 .full-width {
-	width: 100%;
-	margin-left: 0;
-	margin-right: 0;
+	width 100%
+	margin-left 0
+	margin-right 0
 }
 .v-btn {
 	font-weight normal
 	text-transform unset
 }
+.social {
+	display flex
+	justify-content space-between
+
+	span {
+		line-height 50px
+	}
+}
 </style>
 
 <script>
+import SocialBtn from '~/components/Social-Button.vue'
+
 export default {
+	components: {
+		'social-btn': SocialBtn
+	}
 }
 </script>
