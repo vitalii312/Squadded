0330f5f708f80ad5334ad46a30559987496cc64c
simbaking029 <simbaking028@gmail.com>
2019-07-22 12:11:11 +0300

    Social service. OAuth

---
+++ services/social.js
@@ -0,0 +1,33 @@
+const OAUTH = {
+	facebook: {
+		endpoint: 'https://www.facebook.com/v3.3/dialog/oauth?',
+		id: '675899836211443',
+		scope: 'public_profile,email',
+		display: 'popup'
+	},
+};
+
+function generateState () {
+	return Math.random().toString(36).slice(2);
+}
+
+export default class Social {
+	static redirectUrl (providerName) {
+		return `https://api.squad-shopping.com/auth/${providerName}/callback`
+	}
+
+	static oauth (providerName) {
+		if (!OAUTH[providerName]) {
+			return;
+		}
+
+		const uri = OAUTH[providerName].endpoint +
+			`client_id=${OAUTH[providerName].id}` +
+			`&redirect_uri=${Social.redirectUrl(providerName)}` +
+			`&state=${generateState()}` +
+			'&response_type=code' +
+			(OAUTH[providerName].scope ? `&scope=${OAUTH[providerName].scope}` : '') +
+			(OAUTH[providerName].display ? `&display=${OAUTH[providerName].display}` : '');
+		window.open(uri, 'OAuth', 'width=520,height=570');
+	}
+}
