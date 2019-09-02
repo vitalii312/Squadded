bc870d3f80f53f7de43808aa7e2c4cb13b0d12e0
simbaking029 <simbaking028@gmail.com>
2019-09-02 22:36:42 +0300

    Listen merchant id message tests

--- plugins/messaging.spec.js
+++ plugins/messaging.spec.js
@@ -1,4 +1,8 @@
+import fetchMock from 'fetch-mock';
+import merchant from '../services/merchant';
 import messaging, { context } from './messaging';
+
+const { API_LINK } = process.env;
 
 describe('Message listener', () => {
 	it('should add event listener', () => {
@@ -40,5 +44,74 @@ describe('Message listener', () => {
 		expect(store.dispatch).toHaveBeenCalledTimes(1);
 		expect(store.dispatch.calls.argsFor(0)).toEqual(['feed/saveItem', msg]);
 	});
+});
 
+describe('Listen merchant id', () => {
+	const store = {
+		commit: function () {},
+	};
+	const msg = {
+		type: 'injectMerchantId',
+		merchantId: 'aMerchantId',
+	};
+	const event = {
+		data: JSON.stringify(msg),
+	};
+
+	beforeEach(() => {
+		spyOn(store, 'commit');
+		spyOn(merchant, 'validateAllowedOrigins').and.callThrough();
+	});
+	afterEach(fetchMock.reset);
+
+	it('should set state forbidden if origin not allowed for merchantId', async (done) => {
+		window.location.ancestorOrigins = ['http://not.allowed.com'];
+
+		fetchMock.get(`${API_LINK}/merchant/${msg.merchantId}/origins`, {
+			status: 200,
+			body: {
+				list: ['http://allowed.com'],
+			},
+		});
+		const promise = context({ store })(event);
+
+		expect(merchant.validateAllowedOrigins).toHaveBeenCalledTimes(1);
+
+		try {
+			await promise;
+		} catch (error) {
+			expect(error).toEqual(new Error('Forbidden'));
+		}
+
+		expect(store.commit).toHaveBeenCalledTimes(1);
+		expect(store.commit.calls.argsFor(0)).toEqual(['SET_MERCHANT_FORBIDDEN', true]);
+
+		done();
+	});
+
+	it('should not forbidden if origin not allowed for merchantId', async (done) => {
+		window.location.ancestorOrigins = ['http://allowed.com'];
+
+		fetchMock.get(`${API_LINK}/merchant/${msg.merchantId}/origins`, {
+			status: 200,
+			body: {
+				list: ['http://allowed.com'],
+			},
+		});
+
+		const promise = context({ store })(event);
+
+		expect(merchant.validateAllowedOrigins).toHaveBeenCalledTimes(1);
+
+		try {
+			await promise;
+		} catch (error) {
+			expect(error).not.toEqual(new Error('Forbidden'));
+		}
+
+		expect(store.commit).toHaveBeenCalledTimes(1);
+		expect(store.commit.calls.argsFor(0)).toEqual(['SET_MERCHANT_ID', msg.merchantId]);
+
+		done();
+	});
 });
