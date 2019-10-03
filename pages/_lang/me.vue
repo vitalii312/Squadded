28ee752e5923c20cc0558ff0a44a0cfa55f9f650
simbaking029 <simbaking028@gmail.com>
2019-10-03 16:33:34 +0300

    Add dummy my profile page

---
+++ pages/_lang/me.vue
@@ -0,0 +1,10 @@
+<template>
+	<v-layout>
+		<h2>Your profile</h2>
+	</v-layout>
+</template>
+
+<script>
+export default {
+};
+</script>
---
+++ pages/me.vue
@@ -0,0 +1,4 @@
+<script>
+import Me from '~/pages/_lang/me';
+export default Me;
+</script>
