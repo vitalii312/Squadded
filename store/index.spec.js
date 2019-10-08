e2cde33fbbf2962eef850262c9f8400952f888b6
simbaking029 <simbaking028@gmail.com>
2019-10-08 21:07:57 +0300

    Cover root store with test

---
+++ store/index.spec.js
@@ -0,0 +1,45 @@
+import Vuex from 'vuex';
+import { createLocalVue } from '@vue/test-utils';
+import Store from './index';
+
+describe('Root store', () => {
+	let localVue;
+	let store;
+
+	beforeEach(() => {
+		localVue = createLocalVue();
+		localVue.use(Vuex);
+
+		store = new Vuex.Store(Store);
+	});
+
+	it('should set socket open', () => {
+		const event = {
+			currentTarget: 'websocketInstance',
+		};
+		store.commit('SOCKET_ONOPEN', event);
+
+		expect(store.state.socket.isConnected).toBe(true);
+		expect(store.state.socket._ws).toBe(event.currentTarget);
+	});
+
+	it('should set socket', () => {
+		const ws = 'websocketInstance';
+		store.commit('jSocket', ws);
+
+		expect(store.state.socket.$ws).toBe(ws);
+	});
+
+	it('should set socket closed', () => {
+		store.commit('SOCKET_ONCLOSE');
+
+		expect(store.state.socket.isConnected).toBe(false);
+	});
+
+	it('should set merchant id', () => {
+		const merchantId = 'someMerchantId';
+		store.commit('SET_MERCHANT_ID', merchantId);
+
+		expect(store.state.merchant.id).toBe(merchantId);
+	});
+});
