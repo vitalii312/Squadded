a6ceafda317b23f30e80951626c6851b026202c6
simbaking029 <simbaking028@gmail.com>
2019-10-16 21:08:45 +0300

    Rename component tag

--- components/Feed/index.vue
+++ components/Feed/index.vue
@@ -1,7 +1,7 @@
 <template lang="html">
 	<section class="feed">
 		<span v-if="!items.length" ref="empty-feed-text">{{ $t('feed.isEmpty') }}</span>
-		<feed-post
+		<FeedPost
 			v-for="post in items"
 			:key="post.correlationId || post.guid"
 			:post="post"
@@ -15,7 +15,7 @@ import FeedPost from './Post';
 export default {
 	name: 'Feed',
 	components: {
-		'feed-post': FeedPost,
+		FeedPost,
 	},
 	props: {
 		items: {
