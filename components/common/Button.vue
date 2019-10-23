1e088583cd6cd14bb14734081c6ee053bc414a27
simbaking029 <simbaking028@gmail.com>
2019-10-23 17:20:56 +0300

    Fix click and content

--- components/common/Button.vue
+++ components/common/Button.vue
@@ -1,5 +1,5 @@
 <template>
-	<button>Follow</button>
+	<button><slot /></button>
 </template>
 
 <script>
@@ -8,20 +8,19 @@ export default {
 </script>
 
 <style scoped>
-    button {
+	button {
 		padding: 4.5% 14%;
 		margin-top: 5%;
 		margin-left: auto;
 		margin-right: auto;
 
-        background-color: black;
-        border-radius: 10px;
+		background-color: black;
+		border-radius: 10px;
 
 		color: white;
 		text-transform: uppercase;
 		font-size: .6em;
 		font-weight: 700;
 		letter-spacing: 1px;
-    }
-
+	}
 </style>
