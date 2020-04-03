14e557afc1db212aa5825f051415875522d0098c
simbaking029 <simbaking028@gmail.com>
2020-04-03 15:53:39 +0530

    Solve: [Design] Single item #732

--- components/Posts/SingleItemPost.vue
+++ components/Posts/SingleItemPost.vue
@@ -1,10 +1,12 @@
 <template>
-	<Post :post="post">
+	<Post :post="post" group-post class="grouped-post">
 		<ProductCard
 			ref="product-card"
 			class="mx-auto pa-4 w-78 mb-4 single-item"
 			:item="post.item"
+			:post="post"
 			show-refreshicon
+			group-post
 			:loading="!post.guid && !post.error"
 			:post-id="post.guid"
 		/>
