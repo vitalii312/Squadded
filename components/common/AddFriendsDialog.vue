7561913ccff723fc2ffd9c943dbea7b12b935912
simbaking029 <simbaking028@gmail.com>
2020-04-13 15:19:15 -0400

    #805 invite social button

--- components/common/AddFriendsDialog.vue
+++ components/common/AddFriendsDialog.vue
@@ -11,32 +11,19 @@
 		<div class="mb-0">
 			<FindFriends v-if="showDialog" ref="find-friends" />
 		</div>
-		<div class="d-flex black justify-center align-center pa-2 pl-10 copy-link-button">
-			<strong class="white--text subtitle-2 font-weight-bold">
-				{{ $t('mysquad.share_private_link') }}
-			</strong>
-			<Button ref="copy-btn" class="ma-0 ml-4" style="background: #fd6256; height: 9.23vw;" @click.native="copy">
-				{{ copied ? $t('invite_your_friends.copied') : $t('invite_your_friends.copy_link') }}
-			</Button>
-		</div>
-		<input ref="shorturl" v-model="shortURL" class="link-box">
+		<v-divider />
+		<ShareInviteLink class="pt-3 pb-8 px-3" show-title />
 	</v-dialog>
 </template>
 
 <script>
-import { createNamespacedHelpers } from 'vuex';
 import FindFriends from '~/components/common/FindFriends';
-import { copy } from '~/utils/copy';
-import Button from '~/components/common/Button';
-import { UserStore } from '~/store/user';
-import { getShortURL } from '~/services/short-url';
-
-const userState = createNamespacedHelpers(UserStore).mapState;
+import ShareInviteLink from '~/components/common/ShareInviteLink';
 
 export default {
 	components: {
-		Button,
 		FindFriends,
+		ShareInviteLink,
 	},
 	props: {
 		show: {
@@ -45,27 +32,8 @@ export default {
 		},
 	},
 	data: () => ({
-		shortURL: null,
 		showDialog: false,
-		copied: false,
 	}),
-	computed: {
-		...userState(['me']),
-		target() {
-			const { siteUrl, siteTitle } = this.$store.state.merchant;
-			return {
-				id: this.me.userId,
-				url: siteUrl,
-				title: siteTitle,
-				invite: true,
-			};
-		},
-		userLink() {
-			const { API_ENDPOINT } = this.$store.state.squad;
-			const target = JSON.stringify(this.target);
-			return `${API_ENDPOINT}/community/profile?t=${btoa(target)}`;
-		},
-	},
 	watch: {
 		show() {
 			this.showDialog = this.show;
@@ -79,18 +47,8 @@ export default {
 	},
 	mounted() {
 		this.showDialog = this.show;
-		this.setLink();
 	},
 	methods: {
-		async setLink() {
-			this.shortURL = await getShortURL(this.userLink, this.$store);
-		},
-		copy() {
-			this.$refs.shorturl.select();
-			copy();
-			this.copied = true;
-			setTimeout(() => (this.copied = false), 1000);
-		},
 		hide() {
 			this.$emit('close', false);
 		},
@@ -98,12 +56,6 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-.link-box {
-	display: block;
-	height: 0;
-}
-</style>
 <style lang="scss">
 .add-friends-dialog {
 	background: white;
@@ -113,10 +65,5 @@ export default {
 .close-dialog {
 	position: absolute;
 	right: 15px;
-}
-.copy-link-button {
-	background-image:url('../../assets/img/popup-share.svg');
-	background-size: 5.8VW;
-	background-position: 7.5vw center;
 }
 </style>
---
+++ components/common/ShareInviteLink.vue
@@ -0,0 +1,135 @@
+<template>
+	<div>
+		<div v-if="showTitle" class="title text-center mb-4">
+			{{ $t('mysquad.share_private_link') }}:
+		</div>
+		<div class="d-flex justify-space-around">
+			<div
+				v-for="(method, index) of methods"
+				:key="index"
+				class="d-flex flex-column justify-center align-center"
+				style="width: 23vw"
+				@click="() => action(method)"
+			>
+				<div class="method-icon d-flex align-center justify-center">
+					<img v-if="method.title === 'messenger'" :width="30" :height="30" src="~assets/img/messenger.svg">
+					<img
+						v-else-if="method.title === 'whatsapp'"
+						:width="40"
+						:height="40"
+						src="~assets/img/whatsapp.svg"
+					>
+					<img v-else-if="method.title === 'email'" :width="40" :height="40" src="~assets/img/envelope.svg">
+					<img v-else src="~assets/img/copy-link.png" :width="25" :height="25">
+				</div>
+				<div class="mt-2 caption">
+					{{ $t('invite_your_friends.' + method.title) }}
+				</div>
+			</div>
+		</div>
+		<input ref="shorturl" v-model="shortURL" class="link-box">
+	</div>
+</template>
+
+<script>
+import { createNamespacedHelpers } from 'vuex';
+import { getShortURL } from '~/services/short-url';
+import { copy } from '~/utils/copy';
+import { UserStore } from '~/store/user';
+
+const userState = createNamespacedHelpers(UserStore).mapState;
+
+export default {
+	props: {
+		showTitle: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	data: () => ({
+		methods: [
+			{
+				title: 'messenger',
+				icon: 'messenger.svg',
+			},
+			{
+				title: 'whatsapp',
+				icon: 'whatsapp.svg',
+			},
+			{
+				title: 'email',
+				icon: 'envelope.svg',
+			},
+			{
+				title: 'copy_link',
+				icon: 'copy-link.png',
+			},
+		],
+		shortURL: null,
+		copied: false,
+	}),
+	computed: {
+		...userState(['me']),
+		target() {
+			const { siteUrl, siteTitle } = this.$store.state.merchant;
+			return {
+				id: this.me.userId,
+				url: siteUrl,
+				title: siteTitle,
+				invite: true,
+			};
+		},
+		userLink() {
+			const { API_ENDPOINT } = this.$store.state.squad;
+			const target = JSON.stringify(this.target);
+			return `${API_ENDPOINT}/community/profile?t=${btoa(target)}`;
+		},
+	},
+	mounted() {
+		this.setLink();
+	},
+	methods: {
+		async setLink() {
+			this.shortURL = await getShortURL(this.userLink, this.$store);
+		},
+		action(method) {
+			const content = this.$t('invite_your_friends.invite_body', { merchant: this.$store.state.merchant.id });
+			switch (method.title) {
+			case 'messenger':
+				window.open(`https://www.facebook.com/dialog/send?app_id=${process.env.FB_APP_ID}&link=${this.shortURL}&redirect_uri=${window.location.origin}`);
+				break;
+			case 'copy_link':
+				this.$refs.shorturl.select();
+				copy();
+				method.title = 'copied';
+				setTimeout(() => (method.title = 'copy_link'), 1000);
+				break;
+			case 'email':
+				window.open(`mailTo:?subject=${this.$t('invite_your_friends.invite_subject')}&body=${content}%0A${this.shortURL}`);
+				break;
+			case 'whatsapp':
+				window.open(`whatsapp://send?text=${content}`);
+				break;
+			}
+		},
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+.method-icon {
+	width: 13vw;
+	height: 13vw;
+	border-radius: 10px;
+	line-height: 0;
+	box-shadow: -1px 2px 19px 1px rgba(0, 0, 0, 0.07843);
+	cursor: pointer;
+}
+.link-box {
+	display: block;
+	height: 0;
+}
+.caption {
+	font-size: 0.6rem !important;
+}
+</style>
