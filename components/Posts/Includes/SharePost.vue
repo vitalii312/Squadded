3f04cb670fbc0a14d3a573fda09132e398170544
simbaking029 <simbaking028@gmail.com>
2020-06-03 03:59:44 -0400

    #925

--- components/Posts/Includes/SharePost.vue
+++ components/Posts/Includes/SharePost.vue
@@ -11,15 +11,31 @@
 		<v-card-text class="share_url">
 			<v-text-field ref="post-link" :value="postLink" />
 		</v-card-text>
-		<v-btn
-			class="full-width copy_btn"
-			color="primary"
-			large
-			depressed
-			@click="copyPostLink"
-		>
-			Copy Link
-		</v-btn>
+		<div class="d-flex justify-space-around">
+			<div
+				v-for="(method, index) of methods"
+				:key="index"
+				class="d-flex flex-column justify-center align-center"
+				style="width: 23vw"
+				@click="() => action(method)"
+			>
+				<div class="method-icon d-flex align-center justify-center">
+					<img v-if="method.title === 'messenger'" :width="32" :height="32" src="~assets/img/messenger.svg">
+					<img
+						v-else-if="method.title === 'whatsapp'"
+						:width="32"
+						:height="32"
+						src="~assets/img/whatsapp.svg"
+					>
+					<img v-else-if="method.title === 'email'" :width="30" :height="30" src="~assets/img/envelope.svg">
+					<img v-else src="~assets/img/copy-link.svg" :width="25" :height="25">
+				</div>
+				<div class="mt-2 caption">
+					{{ $t('invite_your_friends.' + method.title) }}
+				</div>
+			</div>
+		</div>
+		<input ref="shorturl" v-model="postLink" class="link-box">
 	</v-card>
 </template>
 
@@ -33,13 +49,51 @@ export default {
 			required: true,
 		},
 	},
+	data: () => ({
+		methods: [
+			{
+				title: 'messenger',
+				icon: 'messenger.svg',
+			},
+			{
+				title: 'whatsapp',
+				icon: 'whatsapp.svg',
+			},
+			{
+				title: 'email',
+				icon: 'envelope.svg',
+			},
+			{
+				title: 'copy_link',
+				icon: 'copy-link.svg',
+			},
+		],
+		copied: false,
+	}),
 	methods: {
-		copyPostLink () {
-			this.$refs['post-link'].$el.querySelector('input').select();
-			copy();
-		},
 		hide () {
 			this.$emit('hideShowShare');
+		},
+		action(method) {
+			this.$emit('shared');
+			const content = this.$t('post.share_link.content', { merchant: this.$store.state.merchant.id });
+			switch (method.title) {
+			case 'messenger':
+				window.open(`https://www.facebook.com/dialog/send?app_id=${process.env.FB_APP_ID}&link=${this.postLink}&redirect_uri=${window.location.origin}`);
+				break;
+			case 'copy_link':
+				this.$refs.shorturl.select();
+				copy();
+				method.title = 'copied';
+				setTimeout(() => (method.title = 'copy_link'), 1000);
+				break;
+			case 'email':
+				window.open(`mailTo:?subject=${this.$t('post.share_link.subject', { merchant: this.$store.state.merchant.id })}&body=${content}%0A${this.postLink}`);
+				break;
+			case 'whatsapp':
+				window.open(encodeURI(`https://api.whatsapp.com/send?text=${content}\n${this.postLink}`));
+				break;
+			}
 		},
 	},
 };
@@ -74,4 +128,21 @@ export default {
 		font-weight 700
 		display block
 		border-radius 3.07vw
+</style>
+<style lang="scss" scoped>
+.method-icon {
+	width: 17.84vw;
+	height: 17.84vw;
+	border-radius: 4.92vw;
+	line-height: 0;
+	box-shadow: -1px 2px 19px 1px rgba(0, 0, 0, 0.07843);
+	cursor: pointer;
+}
+.link-box {
+	display: block;
+	height: 0;
+}
+.caption {
+	font-size: 0.6rem !important;
+}
 </style>
