6165825bbfe1f8fb92c260d40cefb29d686e78ea
simbaking029 <simbaking028@gmail.com>
2020-07-15 15:01:22 +0300

    Update create button design

--- components/common/CreateTab.vue
+++ components/common/CreateTab.vue
@@ -1,14 +1,12 @@
 <template>
-	<v-tab :disabled="!socket.isAuth" class="create tab_item" @click="toggleMenu">
+	<v-tab :disabled="!socket.isAuth" class="tab_item" @click="toggleMenu">
 		<v-menu top offset-y>
 			<template v-slot:activator="{ on }">
-				<v-btn ref="createTabBtn" icon v-on="on">
-					<v-icon
-						class="tab_icon plus_icon"
-					>
-						sqdi-plus
+				<span ref="createTabBtn" class="tab_icon" v-on="on">
+					<v-icon>
+						sqdi-add-post
 					</v-icon>
-				</v-btn>
+				</span>
 			</template>
 
 			<v-list class="open-add-section">
@@ -58,8 +56,13 @@ export default {
 		});
 	},
 	methods: {
-		toggleMenu () {
-			this.$root.$emit('overlayToggle', {});
+		toggleMenu (event) {
+			const icon = event.target.closest('.tab_icon');
+			if (!icon) {
+				this.$refs.createTabBtn.click();
+			} else {
+				this.$root.$emit('overlayToggle', {});
+			}
 		},
 		closeMenu (uri) {
 			const { path } = this.$route;
@@ -73,35 +76,22 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-.v-tab{
-	min-width: auto;
-	padding :0;
-}
-.create.tab_item.v-tab{
-	margin-top: -5vw;
-}
-@media screen and (max-width: 280px){
-	.create.tab_item.v-tab{
-		margin-top: -9vw;
-	}
-}
+<style lang="stylus" scoped>
+.v-tab
+	min-width auto
+	padding 0
 
-.v-tabs-bar__content .create.tab_item .v-btn{
-	content: "";
-	z-index: 5;
-	border: 0px solid black;
-	border-radius: 6.153vw;
-	height: 18.461vw;
-	width: 18.461vw;
-	min-width:auto;
-	background: var(--brand-color);
-	margin-top: 2vw;
-}
-.plus_icon:before{
-	color : #fff;
-	font-size : 3.846vw !important;
-}
+.tab_icon
+	position relative
+	.v-icon:before
+		font-size 1.46em
+		color #B8B8BA
+
+.v-tab--active
+	.tab_icon .v-icon:before,
+	.tab_text
+		color var(--brand-color)
+
 .v-menu__content.theme--light.v-menu__content--fixed.menuable__content__active {
 	min-width: 100% !important;
 	width: 100% !important;
@@ -124,45 +114,45 @@ export default {
 .v-list.open-add-section.v-sheet.v-sheet--tile.theme--light {
 	padding: 4.893vw 4VW 0;
 	padding-bottom: 5.6vw;
-    border-radius: 0px !important;
+	border-radius: 0px !important;
 }
 .v-list.open-add-section.v-sheet.v-sheet--tile.theme--light h5 {
-    color: #B8B8BA;
-    font-size: 2.933vw;
-    font-weight: 500;
-    padding-bottom: 3.826vw;
+	color: #B8B8BA;
+	font-size: 2.933vw;
+	font-weight: 500;
+	padding-bottom: 3.826vw;
 }
 .open-add-section .v-list-item.theme--light .link-title {
-    color: #000;
-    font-size: 3.466vw;
-    font-weight: 700;
+	color: #000;
+	font-size: 3.466vw;
+	font-weight: 700;
 	width: 100%;
-    display: flex;
-    align-items: center;
+	display: flex;
+	align-items: center;
 }
 .open-add-section .v-list-item.theme--light {
-    padding: 0;
-    display: flex;
+	padding: 0;
+	display: flex;
 	align-items :center;
-    padding-bottom: 3.280vw;
-    border-bottom: 0.4vw solid #DBDBDB;
+	padding-bottom: 3.280vw;
+	border-bottom: 0.4vw solid #DBDBDB;
 	padding-top: 3.28vw;
 	cursor: pointer;
 }
 .open-add-section .v-list-item.theme--light.Outfit {
-    padding-top: 0px;
+	padding-top: 0px;
 }
 .discription {
-    color: #B8B8BA;
-    font-size: 2.80vw;
-    font-weight: normal;
+	color: #B8B8BA;
+	font-size: 2.80vw;
+	font-weight: normal;
 }
 .left-content-sec {
-    padding-right: 4VW;
+	padding-right: 4VW;
 	height: 5.266vw;
 }
 .left-content-sec img {
-    width:  5.266vw;
-    height:  5.266vw;
+	width:  5.266vw;
+	height:  5.266vw;
 }
 </style>
